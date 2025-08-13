aa2ff0b630037b379ff0a5113d4580d31884a6be
Davi Pedrosa <davipedrosassss@gmail.com>
2025-08-13 16:23:46 -0300

    Primeiras alterações nos arquivos e adição dos arquivos Protegida e Login

--- MauiAppLogin/App.xaml.cs
+++ MauiAppLogin/App.xaml.cs
@@ -5,11 +5,45 @@
         public App()
         {
             InitializeComponent();
+
+            MainPage = new AppShell();
         }
 
+        /* Método abaixo, é utilizado na personalização e configurações das janelas de nosso app, definindo alguns atributos
+        como tamanho, largura, posição, título e ate múltiplas janelas
+        
+         --- Descrição de seu parâmetro ----
+        
+         * activationState: Informações sobre o estado de ativação do app (podendo ser nulo)
+        
+         --- Possíveis chamadas ---
+        
+         * Tamanho e largura:       window.Height = 600;
+                                    window.Width = 400;
+        
+         * Definir título e posição inicial:          window.Title = "Minha aplicação de Login em .MAUI";
+                                                      window.X = 100;
+                                                      window.Y = 100;
+        
+         * Criando múltiplas janelas:       var mainWindow = new Window(new MainPage());
+    var                                     secondaryWindow = new Window(new SecondaryPage());
+                                            Lógica que gerenciará as janelas
+                                            return mainWindow;
+        */
         protected override Window CreateWindow(IActivationState? activationState)
         {
-            return new Window(new AppShell());
+            /* Sempre chame base.CreateWindow(activationState) se quiser manter o comportamento padrão e apenas ajustar propriedades,
+            isso porque, como o .MAUI trabalha com Android e IOS, nessas, existem apenas 1 página, uma vez que telefones não são tão
+            dinâmicos como computadores, que permitem termos mais de ao mesmo tempo.
+            
+             Sendo assim, ao utilizarmos o base.CreateWindow(activationState) faremos com que nessas plataformas os padrões do sistema
+            sejam respeitados e alterando apenas algumas propriedades chaves*/
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
         }
     }
 }
